4a4dabe8e835c3e039e9ce77605b48edff710819
Quynh <hp.quynh@yahoo.com>
2021-05-30 18:47:45 -0700

    create vue client

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
-from flask import Flask, render_template, url_for, redirect, request, send_from_directory
+from flask import Flask, render_template, url_for, redirect, request, send_from_directory, jsonify
+from flask_cors import CORS
 import json
 import os
 from datetime import date
 import requests
 
 from secrets import twitter_token, base_url, search_url, token2
-now = date.today()  # end date
 
-places = {'Seattle':'2490383',
+places = {'Seattle': '2490383',
           'Toronto': '4118',
-           'Sydney':'1105779',
-           'London':'44418'}
-headers={"Authorization": "Bearer {}".format(twitter_token)}
-headers2={"Authorization": "Bearer {}".format(token2)}
+          'Sydney': '1105779',
+          'London': '44418'}
 
+headers = {"Authorization": "Bearer {}".format(twitter_token)}
+headers2 = {"Authorization": "Bearer {}".format(token2)}
+
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
 
 @app.route('/')
 def hello_world():
     return render_template('base.html', data=[], place="")
 
+
 @app.route('/home', methods=['GET'])
 def back_home():
     return redirect(url_for('hello_world'));
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='favicon.ico')
+
 
 @app.route('/<place>', methods=['GET'])
 def get_trends_today(place):
@@ -38,14 +54,15 @@ def get_trends_today(place):
     # render_template('list.html', data=tweet_data, place=place)
     return render_template('base.html', data=tweet_data, place=place);
 
+
 @app.route('/<place>/<query>', methods=['GET'])
 def search(place, query):
-    query_url=search_url.format(query)
-    res=requests.get(query_url, headers=headers)
-    tweet_data=res.json()
+    query_url = search_url.format(query)
+    res = requests.get(query_url, headers=headers)
+    tweet_data = res.json()
     render_template('tweet_list.html', data=tweet_data['statuses'], query=query)
     return redirect(url_for('hello_world'));
 
+
 if __name__ == '__main__':
-    app.jinja_env.auto_reload = True
-    app.run(debug=True)
+    app.run()
