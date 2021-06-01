72e2c8c2ded9fabefd8d3ef3a5e3e599fa485554
Quynh <hp.quynh@yahoo.com>
2021-05-31 22:53:58 -0700

    sentimental analysis and select tweet

--- client/src/components/Trends.vue
+++ client/src/components/Trends.vue
@@ -12,20 +12,50 @@
           <h2>Today's trends</h2>
           <p> Select location to see trending Twitter topics: </p>
           <div>
-            <b-button-group>
-              <b-button @click="onGetTrends('Seattle')">Seattle</b-button>
-              <b-button @click="onGetTrends('Sydney')">Sydney</b-button>
-              <b-button @click="onGetTrends('London')">London</b-button>
-              <b-button @click="onGetTrends('Toronto')">Toronto</b-button>
-            </b-button-group>
-            <br><br>
-            <b-table hover :items="trends" :fields="fields" v-if="showTrends"
-                     @click="onGetTweets('${trends.name}')"></b-table>
-<!--https://bootstrap-vue.org/docs/components/table#jane-doe-->
+            <b-form-group>
+              <b-form-radio-group
+                id="btn-radios-2"
+                v-model="selected0"
+                button-variant="outline-primary"
+                name="radio-btn-outline"
+                :options = "options"
+                buttons
+                @click="onGetTrends(selected0)">
+              </b-form-radio-group>
+            </b-form-group>
+            <br>
+            <b-table hover
+                     :items="trends" :fields="fields"
+                     :select-mode="selectMode"
+                      responsive="sm"
+                      ref="selectableTable"
+                      selectable
+                      :tbody-tr-class="rowClass"
+                      @row-selected="onRowSelected">
+            </b-table>
           </div>
         </b-col>
-        <b-col>2 of 3</b-col>
-        <b-col>3 of 3</b-col>
+        <b-col>
+          <h3 v-if="showTweets">Tweets Sample on {{selected}}</h3>
+          <b-list-group v-for="(tweet, index) in tweets" :key="index">
+            <b-list-group-item href="#" class="flex-column align-items-start"
+                               v-bind:class="{ 'active' : isSelected(tweet) }"
+                               v-on:click="selected2 = tweet">
+              <div class="d-flex w-100">
+                <small style="padding-left: 240px">
+                  {{ tweet.created_at | subStr }} </small>
+              </div>
+              <p class="mb-1"> {{ tweet.text }} </p>
+            </b-list-group-item>
+          </b-list-group>
+        </b-col>
+        <b-col>
+          <h2>Sentiment Analysis</h2>
+          <p> {{selected2.text}} </p>
+          <p> {{sentAna.assessment}} </p>
+          <p> {{sentAna.polarity}} </p>
+          <p> {{ sentAna.subjectivity}} </p>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -38,9 +68,33 @@ export default {
   data() {
     return {
       trends: [],
-      showTrends: false,
+      showTweets: false,
       fields: [],
+      selectMode: 'single',
+      selected: '',
+      tweets: [],
+      selected2: '',
+      selected0: '',
+      sentAna: '',
+      options: [
+        { text: 'Seattle', value: 'Seattle' },
+        { text: 'Toronto', value: 'Toronto' },
+        { text: 'Sydney', value: 'Sydney' },
+        { text: 'London', value: 'London' }],
     };
+  },
+  filters: {
+    subStr(string) {
+      return string.substring(0, 10);
+    },
+  },
+  watch: {
+    selected0(newVal) {
+      this.getTrends(newVal);
+    },
+    selected2(newVal) {
+      this.getSent(newVal);
+    },
   },
   methods: {
     getTrends(location) {
@@ -48,7 +102,6 @@ export default {
       axios.get(path)
         .then((res) => {
           this.trends = res.data.trends;
-          this.showTrends = true;
           this.fields = ['name', 'tweet_volume'];
         })
         .catch((error) => {
@@ -57,14 +110,51 @@ export default {
         });
     },
     getTweets(query) {
-      console.log(query);
+      const path = 'http://localhost:5000/tweets/';
+      axios.post(path, query)
+        .then((res) => {
+          this.tweets = res.data.tweets.data;
+          this.showTweets = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+        });
+    },
+    getSent(text) {
+      const path = 'http://localhost:5000/sentiment/';
+      axios.post(path, text)
+        .then((res) => {
+          this.sentAna = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+        });
     },
     onGetTrends(location) {
       this.getTrends(location);
     },
-    onGetTweets(query) {
-      this.getTweets(query);
+    rowClass(item) {
+      if (this.selected.includes(item)) {
+        return 'table-success';
+      }
+      return '';
+    },
+    onRowSelected(items) {
+      this.selected = items[0].name;
+      this.getTweets(items[0]);
+    },
+    isSelected(i) {
+      return i === this.selected2;
     },
   },
 };
 </script>
+
+<style>
+.list-group {
+  padding-bottom: 7px;
+  border-radius: 0.5rem;
+}
+</style>
--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, url_for, redirect, request, send_from_directory, jsonify
 from flask_cors import CORS
-import json
-import os
-from datetime import date
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('spacytextblob')
+
 import requests
 
-from secrets import twitter_token, base_url, search_url, token2
+from secrets import twitter_token, base_url, search_url, token2, search_url2
 
 places = {'Seattle': '2490383',
           'Toronto': '4118',
@@ -35,9 +38,18 @@ def ping_pong():
 #     return render_template('base.html', data=[], place="")
 
 
-# @app.route('/home', methods=['GET'])
-# def back_home():
-#     return redirect(url_for('hello_world'));
+@app.route('/sentiment/', methods=['POST'])
+def spacy_sent():
+    if request.method == 'POST':
+        post_data = request.get_json()
+    print(post_data)
+    doc=nlp(post_data.get('text'))
+    return jsonify({
+        'status': 'success',
+        'polarity': doc._.polarity,      # Polarity: [-1,1]
+        'subjectivity': doc._.subjectivity, # Sujectivity: 0.9
+        'assessment': doc._.assessments,
+    })
 
 
 @app.route('/trends/<location>', methods=['GET'])
@@ -58,14 +70,33 @@ def get_trends_today(location):
     })
 
 
-# @app.route('/<place>/<query>', methods=['GET'])
-# def search(place, query):
-#     query_url = search_url.format(query)
-#     res = requests.get(query_url, headers=headers)
-#     tweet_data = res.json()
-#     render_template('tweet_list.html', data=tweet_data['statuses'], query=query)
-#     return redirect(url_for('hello_world'));
-#
+@app.route('/tweets/', methods=['GET', 'POST'])
+def search():
+    tweet_fields = "tweet.fields=author_id,created_at"
+    if request.method == 'POST':
+        post_data = request.get_json()
+    query = parse_query(post_data.get('name'))
+    query_url = search_url2.format(query, tweet_fields)
+    print(query_url)
+    res = requests.get(query_url, headers=headers2)
+    tweet_data = res.json()
+    print(tweet_data)
+    return jsonify({
+        'status': 'success',
+        'tweets': tweet_data
+    })
+
+
+def parse_query(query):
+    parsed = ''
+    for i in query:
+        if i == ' ':
+            parsed += '%20'
+        elif i == '#':
+            parsed += '%23'
+        else:
+            parsed += i
+    return parsed
 
 if __name__ == '__main__':
     app.run()
